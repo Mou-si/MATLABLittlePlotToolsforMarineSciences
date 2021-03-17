6e017091ca2fded7fb925c8fe20ea385c6fefafe
Mou-si <70570276+Mou-si@users.noreply.github.com>
2021-03-17 15:30:36 +0800

    fix a bug
    fix:
    loss of end-/start-point ticks when there is no arrow

--- ColorbarNCLLike.m
+++ ColorbarNCLLike.m
@@ -1,90 +1,140 @@
-% MATLAB script:  Ekman spiral
-% ATTENTION % 
+function h = ColorbarNCLLike(varargin)
+% set colorbar like NCL style, including arrows (via function
+% ColorbarArrowOuter & ColorbarArrowIner), auto remapping, and support
+% auto-adaption with contour plot. The return value is axes of arrows.
+%
+% We hope you can use ColorbarArrowOuter, ColorbarArrowIner more than this
+% fuction
+%
+%% Syntax
+% ColorbarNCLLike
+% h = ColorbarNCLLike;
+% h = ColorbarNCLLike(Name, Value);
+%
+%% varargin lists
+% color            input a n-3 matrix in [0, 1] as colormap, or we will get
+%                  colormap in axes auto
+% Levels           the data valume of colors symbolize, defult is uniform
+% Continuity       the continuity of colorbar, if 1, the arrow will in the
+%                  colorbar like NCL, else out of the colorbar (defult).
+% NewLevelNum      number of levels in Remapping. If continuity, it is 256
+%                  in defult; if uncontinuity and 'Levels' of colormap is
+%                  not uniform, it is 16 in deflut; if uncontinuity but
+%                  there is contour it will also get the level lists
+%                  defultly.
+% Arrow            the varargin of arrows, e.g. ax, up, left, etc.You can
+%                  see funtion ColorbarArrowOuter (continuity) and
+%                  ColorbarArrowIner (uncontinuity) in detail.
+% ColorbarVar      other colorbar varargin can be set here also.
+%
+%% example
+% figure
+% a = [1,1;2,2];
+% h = contourf([1, 2], [1, 2], a);
+% colorbar
+% ColorbarNCLLike
 
-clear all
-close all
-Nxl = 100;
-Lx = 100;                    % water depth (m)
-Nx = Nxl+1;                  % number of grid points
-x = linspace(0,Lx,Nx);
-dx = Lx/Nxl;                 % grid increment in x
-dt = 30;                     % time step in seconds
-cor = 1e-4;                  % coriolis parameter (s-1)
-rho=1000;                    % water density
-tau = 1.0;                   % wind stress (N/m2)
-Kd = 1e-2;                   % diffusion coefficient (m2/s)
-S = fix(960*3600)/dt;         % time steps for 4 day simulation
-r = Kd*dt/dx^2;              % r < 1 for stability
-ff = cor*dt;
+%% input
+if mod(length(varargin), 2)
+    error('Error input')
+end
+ColorbarVar = cell(0);
+Continuity = 0;
+if ~Continuity
+    try % 要是能找到contour，且不连续colorbar，就用他的levels
+        h_contour = findobj(gcf, 'type', 'contour');
+        NewLevelNum = size(h_contour.LevelList, 2) - 1;
+    end
+else % 连续就256
+    NewLevelNum = 256;
+end
+hColorbar = findobj(gcf, 'Type', 'colorbar');
+if isempty(hColorbar)
+    hColorbar = colorbar; % 没有colorbar就创建
+end
+color = colormap;
+Levels = hColorbar.Limits; % colormap defult 按上下限祥均匀分布
+Levels = linspace(Levels(1), Levels(end), size(color, 1) + 1)';
 
-% forward difference in time
-ConvergenceTime = zeros(20);
-initialUV = cat(3, repmat(0 : 0.05 : 0.95, 20, 1), repmat((0 : 0.05 : 0.95)', 1, 20));
-parfor initialUi = 1 : 20
-    for initialVi = 1 : 20
-        t = 0;
-        u1=[];v1=[];t1=[];
-        u = zeros(Nx,1) + initialUV(initialUi, initialVi, 1);
-        v = zeros(Nx,1) + initialUV(initialUi, initialVi, 2);   % initial value
-        un = zeros(Nx, 1);
-        vn = zeros(Nx, 1);
-        for s = 1:S
-            % interior
-            un(2:Nxl) = u(2:Nxl)+ff*v(2:Nxl)+r*(u(3:Nx)-2*u(2:Nxl)+u(1:Nx-2));
-            vn(2:Nxl) = v(2:Nxl)-ff*u(2:Nxl)+r*(v(3:Nx)-2*v(2:Nxl)+v(1:Nx-2));
-            % surface boundary condition
-            un(Nx) = u(Nx)+ff*v(Nx)+(tau/rho)*dt/dx-r*(u(Nx)-u(Nxl));
-            vn(Nx) = v(Nx)-ff*u(Nx)-r*(v(Nx)-v(Nxl));
-            % bottom boundary condition
-            % zero stress
-%             un(1) = u(1)+ff*v(1)+r*(u(2)-u(1));
-%             vn(1) = v(1)-ff*u(1)+r*(v(2)-v(1));
-            % zero velocity
-            un(1)=0;
-            vn(1)=0;
-            % update
-            t=t+dt;
-            u = un; v = vn;
-            if(mod(t,1800)==0)          % save every half hour
-                u1=[u1,u];v1=[v1,v];t1=[t1;t];
-            end
-        end
-        % 收敛
-        
-        sumU = sum(u1,1);
-        sumV = sum(v1,1);
-        for i = 300 : length(u1) - 2 * 10
-            stdV = var(sumU(i : i + 2 * 10)) + var(sumV(i : i + 2 * 10));
-            if stdV < 0.01
-                ConvergenceTime(initialUi, initialVi) = i / 2;
-                break
-            end
-        end
+for i = 1 : length(varargin) / 2
+    switch varargin{i * 2 - 1}
+        case 'color'
+            color = varargin{i * 2};
+        case 'Levels'
+            Levels  = varargin{i * 2};
+        case 'NewLevelNum'
+            NewLevelNum  = varargin{i * 2};
+        case 'Continuity'
+            Continuity = logical(varargin{i * 2});
+        case 'Arrow'
+            Arrow = varargin{i * 2};
+        otherwise
+            ColorbarVar = [ColorbarVar, varargin{i * 2 - 1, i * 2}];
     end
 end
-% %
-% figure
-% plot(t1/3600,sumU*dx,t1/3600,sumV*dx,'r');grid
-% xlabel('Time (hour)')
-% ylabel('Transport (m^2/s)')
-% figure
-% plot(mean(u1),mean(v1),'k-o')
-% set(gca,'dataaspectratio',[1 1 1]);
-% xlabel('Velocity (m/s)');
-% ylabel('Velocity (m/s)');
-% axis([-0.2 0.8 -0.8 0.2])
-% set(gca,'ytick',[-0.8:0.2:0.2])
-% figure
-% plot(mean(u1),x,'r-o',mean(v1),x,'b-o')
-%
-% figure
-% quiver3(zeros(1 ,44), zeros(1 ,44), x(end - 43 : end), ...
-%     u(end - 43 : end), v(end - 43 : end), zeros(1, 44))
+if ~exist('NewLevelNum', 'Var') % 要是没有指定remap后colorbar颜色数
+    if sum(Levels ~= linspace(Levels(1), Levels(end), size(Levels, 1)))
+        NewLevelNum = size(Levels, 1); % 要是原来就是均匀分布就不动
+    else
+        NewLevelNum = 16; % 不均匀就分成16份
+    end
+end
+if size(Levels, 1) == size(color, 1) + 1
+elseif size(Levels, 1) == size(color, 1)
+    Levels = [Levels; 2 * Levels(end) - Levels(end - 1)];
+else
+    Levels = linspace(Levels(1), Levels(end), size(color, 1) + 1)';
+end
 
-figure
-contourf(ConvergenceTime)
-h_contour = findobj(gcf, 'type', 'contour');
-set(h_contour, 'LevelStep', 24)
-colormap(jet)
-set(gca, 'CLim', [600 - 24, 816 + 24])
-ColorbarNCLLike
+%% colorbar level remap
+if NewLevelNum ~= size(Levels, 1)
+    color = ColorbarRemap(color, Levels, NewLevelNum, Continuity);
+end
+
+%% draw colorbar
+colormap(color)
+if ~Continuity
+    try
+        % 设置levels和color对齐，在有箭头的那一端不要tick
+        Levels = linspace...
+            (hColorbar.Limits(1), hColorbar.Limits(2), NewLevelNum + 1);
+        UpLowIndex = [1, size(Levels, 1)];
+        for i = 1 : length(Arrow) / 2
+            switch Arrow{i * 2 - 1}
+                case {'up', 'right'}
+                    if ~varargin{i * 2}
+                        UpLowIndex(UpLowIndex == 1) = [];
+                    end
+                case {'low', 'left'}
+                    if ~varargin{i * 2}
+                        UpLowIndex(UpLowIndex == size(Levels, 1)) = [];
+                    end
+            end
+        end
+        for i = UpLowIndex
+            Levels(i) = NaN;
+        end
+        Levels(isnan(Levels)) = [];
+        h = ColorbarArrowIner(Arrow);
+    catch
+        Levels(UpLowIndex) = [];
+        h = ColorbarArrowIner;
+    end
+    set(hColorbar, 'Ticks', Levels);
+    ColorbarTickLength;
+else
+    try
+        h = ColorbarArrowOuter(Arrow);
+    catch
+        h = ColorbarArrowOuter;
+    end
+end
+
+if ~isempty(ColorbarVar)
+    set(hColorbar, ColorbarVar);
+end
+
+if nargout == 0
+    clear h
+end
+end
