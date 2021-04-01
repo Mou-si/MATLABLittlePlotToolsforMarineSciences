07aa3055b73d148d5a8bcb062402235cb68061be
Mou-si <70570276+Mou-si@users.noreply.github.com>
2021-04-01 19:32:11 +0800

    Add files via upload

---
+++ AxPosition2FigPosition.m
@@ -0,0 +1,61 @@
+function [x, y] = AxPosition2FigPosition(varargin)
+% Convert Axes Position to Figure Position.
+%
+% you should input x or y or both , but you must point them respectively,
+% and you should input one at least. x/y can be number, vector, matrix
+%
+%% Syntax
+% [x, y] = AxPosition2FigPosition(Name, Value)
+%
+%% varargin lists
+% ax                point a axes (gca defult)
+% x                 point a number/vector/matrix of x in ax
+% y                 point a number/vector/matrix of y in ax
+% SkipWarning       skip all warning in this function
+%
+%% example:
+% figure
+% ax1 = axes('position', [0, 0, 0.5, 0.5]);
+% [x, y] = AxPosition2FigPosition('ax', ax1, 'x', 1.5, 'y', 1.2);
+
+%% input
+% check varargin num
+if mod(length(varargin), 2) ~= 0
+    error('Please check input var');
+end
+ax1 = gca;
+SkipWarning = 0;
+for i = 1 : length(varargin) / 2
+    switch varargin{i * 2 - 1}
+        case 'ax'
+            ax1 = varargin{i * 2};
+        case 'x'
+            x = varargin{i * 2};
+        case 'y'
+            y = varargin{i * 2};
+        case 'SkipWarning'
+            SkipWarning = varargin{i * 2};
+    end
+end
+ax1Position = get(ax1, 'Position');
+XLim = get(ax1, 'XLim');
+YLim = get(ax1, 'YLim');
+if ~exist('x', 'Var') && ~exist('y', 'Var')
+    error('Please input x or y')
+end
+%% concalution
+try
+    x = (x - XLim(1)) .* (ax1Position(3) / (XLim(1) - XLim(2))) + ax1Position(1);
+catch
+    if ~SkipWarning
+        warning('have no x input')
+    end
+end
+try
+    y = (y - YLim(1)) .* (ax1Position(4) / (YLim(1) - YLim(2))) + ax1Position(2);
+catch
+    if ~SkipWarning
+        warning('have no y input')
+    end
+end
+end
---
+++ LocationCut.m
@@ -0,0 +1,61 @@
+function [LxLim, LyLim] = LocationCut(SLon, SLat, LLon, LLat, varargin)
+% this function is used to cut some grids from a large gridfile refering to
+% a smaller gridfile. If there is only one output, this function will give
+% a logical matrix that shows the position of cutted grids; if output is
+% two, it will give the limit of x-axis and y-axis.
+%
+% We indicate grid location with complex number by Euler function. So that
+% the the discontinuity of latitude can be avoid with features of
+% multivalue complex function.
+%
+%% Syntax
+% LPosit = LocationCut(SLon, SLat, LLon, LLat);
+% LPosit = LocationCut(..., 'rotation', rotation);
+%
+%% varargin lists
+% SLon          longitude of smaller gridfile (MUST)
+% SLat          latitude of smaller gridfile (MUST)
+% LLon          longitude of larger gridfile (MUST)
+% LLat          latitude of larger gridfile (MUST)
+% rotation      rotation angle of longitude. You can add an angle on 
+%               longitude artificially to rotate the map. When the prime 
+%               meridian locad at diagonal line, the varargin is useful.
+%
+%% example:
+% SIClim = [260, 360; 140, 220] .* 4;
+% SICLon = hdfread('./ExampleData/LonLat-s3125-Antarctic3125.hdf', 'Longitudes');
+% SICLon = SICLon(SIClim(2, 1): SIClim(2, 2), SIClim(1, 1) : SIClim(1, 2));
+% SICLat = hdfread('./ExampleData/LonLat-s3125-Antarctic3125.hdf'], 'Latitudes');
+% SICLat = SICLat(SIClim(2, 1): SIClim(2, 2), SIClim(1, 1) : SIClim(1, 2));
+% DriftLon = ncread('./ExampleData/DTUIceDriftSARExample.nc', 'lon');
+% DriftLat = ncread('./ExampleData/DTUIceDriftSARExample.nc', 'lat');
+% DriftPosition = LocationCut(SICLon, SICLat, DriftLon, DriftLat);
+
+%% input
+for i = 1 : length(varargin) / 2
+    switch varargin{i * 2 - 1}
+        case 'rotation'
+            rotation = varargin{i * 2};
+    end
+end
+
+%% LonLat 2 complex number
+SPosit = SLat .* exp(1i .* (SLon + rotation ./ 180));
+SPositReal = real(SPosit);
+SPositImag = imag(SPosit);
+LPosit = LLat .* exp(1i .* (LLon ./ 180));
+LPositReal = real(LPosit);
+LPositImag = imag(LPosit);
+
+%% Get Position
+LPositReal = LPositReal <= max(SPositReal(:)) & LPositReal >= min(SPositReal(:));
+LPositImag = LPositImag <= max(SPositImag(:)) & LPositImag >= min(SPositImag(:));
+LPosit = LPositReal & LPositImag;
+
+if nargout == 2
+    LxLim = [find(sum(LPosit, 1) > 0, 1), find(sum(LPosit, 1) > 0, 1, 'last')];
+    LyLim = [find(sum(LPosit, 2) > 0, 1), find(sum(LPosit, 2) > 0, 1, 'last')];
+else
+    LxLim = LPosit;
+end
+end
