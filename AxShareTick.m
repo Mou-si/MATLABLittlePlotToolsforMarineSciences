aba710186738acee972bd6e37f43a6c60f816e4e
Mou-si <70570276+Mou-si@users.noreply.github.com>
2021-10-12 12:37:34 +0800

    Add files via upload

--- AxShareTick.m
+++ AxShareTick.m
@@ -45,6 +45,27 @@ function AxShareTick(varargin)
 %                   or 'both'
 %   JustifyCenter   set if the subplots in the last raw will be justify
 %                   center. Input 0 or 1 please. 0 defult.
+%   m_map           a logical value to tell the function if you use m_map
+%                   tool box. 0 defult. ATTETION: before turn on the
+%                   'm_map' switch, check you have inserted following
+%                   command in the main of function m_grid and call m_grid.
+%                       global MAP_GRID
+%                       MAP_GRID.box = gbox;
+%                       MAP_GRID.xtick = xtick;
+%                       MAP_GRID.ytick = ytick;
+%                       MAP_GRID.ticklen = gticklen;
+%                       MAP_GRID.tickdir = gtickdir;
+%                       MAP_GRID.xlabeldir = gxticklabeldir;
+%                       MAP_GRID.ylabeldir = gyticklabeldir;
+%                       MAP_GRID.color = gcolor;
+%                       MAP_GRID.linewidth = glinewidth;
+%                       MAP_GRID.linestyle = glinestyle;
+%                       MAP_GRID.fontsize = gfontsize;
+%                       MAP_GRID.fontname = gfontname;
+%                       MAP_GRID.XaxisLocation = gxaxisloc;
+%                       MAP_GRID.YaxisLocation = gyaxisloc;
+%                   it should be inserted after read the varargin in the
+%                   m_grid (about Line 176).
 %
 %% example
 % for i = 6
@@ -55,7 +76,12 @@ function AxShareTick(varargin)
 %% input
 ax = 'all';
 JustifyCenter = 0;
+m_map = 0;
 for i = 1 : length(varargin)
+    if ~isequal(class(varargin{i}), 'char') && ...
+            ~isequal(class(varargin{i}), 'string')
+        continue
+    end
     switch varargin{i}
         case 'ax'
             ax = varargin{i + 1};
@@ -69,6 +95,8 @@ for i = 1 : length(varargin)
             YAxisLocation = varargin{i + 1};
         case 'JustifyCenter'
             JustifyCenter = varargin{i + 1};
+        case 'm_map'
+            m_map = varargin{i + 1};
     end
 end
 
@@ -133,15 +161,28 @@ else % 手动
 end
 
 % 自动获取AxisLocation
-if ~exist('XAxisLocation', 'Var')
-    XAxisLocation = ax(1).XAxisLocation;
-end
-if length(ax(1).YAxis) == 1
-    if ~exist('YAxisLocation', 'Var')
-        YAxisLocation = ax(1).YAxisLocation;
+if ~m_map
+    if ~exist('XAxisLocation', 'Var')
+        XAxisLocation = ax(1).XAxisLocation;
     end
-elseif length(ax(1).YAxis) == 2
-    YAxisLocation = 'left&right';
+    if length(ax(1).YAxis) == 1
+        if ~exist('YAxisLocation', 'Var')
+            YAxisLocation = ax(1).YAxisLocation;
+        end
+    elseif length(ax(1).YAxis) == 2
+        YAxisLocation = 'left&right';
+    end
+else
+    global MAP_GRID
+    if isempty(MAP_GRID)
+        error('you should insert VAR MAP_GRID and call m_grid. See more in help.')
+    end
+    if ~exist('XAxisLocation', 'Var')
+        XAxisLocation = MAP_GRID.XaxisLocation;
+    end
+    if ~exist('YAxisLocation', 'Var')
+        YAxisLocation = MAP_GRID.YaxisLocation;
+    end
 end
 
 % 根据AxisLocation决定保留哪些标签
@@ -155,6 +196,8 @@ switch XAxisLocation
     case 'both' % 都有情况下要是只有一行就只保留下标签
         XAxisKeep(1 : xn) = 2;
         XAxisKeep(xn * (yn - 1) + 1 : end) = 1;
+    case 'middle'
+        XAxisKeep(:) = 99;
 end
 % 0 -- 无标签； 1 -- 左标签； 2 -- 右标签
 % 只有一个y坐标时一列就够了，多的一列是为了两个坐标
@@ -170,10 +213,12 @@ switch YAxisLocation
         YAxisKeep(end, 1) = 2;
         YAxisKeep(1 : xn : end) = 1;
     case 'left&right'
-        YAxisKeep(1 : xn : end, 1) = 1;
+        YAxisKeep(1 : xn : end, 1) = 99;
         % 跳过指定YAxisLocation，要不然会报错
         YAxisKeep(xn : xn : end, 2) = 99;
         YAxisKeep(end, 2) = 99;
+    case 'middle'
+        YAxisKeep(:) = 99;
 end
 
 % 最后一行不够的图居中对齐
@@ -189,29 +234,70 @@ for i = 1 : length(ax)
     ax(i).Position = [xstart + mod(i - 1, xn) * (xgap + xlength), ...
         ystart - floor((i - 0.5) / xn) * (ygap + ylength), ...
         xlength, ylength];
-    for j = length(ax(1).YAxis)
-        if length(ax(1).YAxis) == 2 && j == 1
-            yyaxis(ax(i), 'left')
-        elseif length(ax(1).YAxis) == 2 && j == 2
-            yyaxis(ax(i), 'right')
+    if ~m_map
+        for j = 1 : length(ax(1).YAxis)
+            if length(ax(1).YAxis) == 2 && j == 1
+                yyaxis(ax(i), 'left')
+            elseif length(ax(1).YAxis) == 2 && j == 2
+                yyaxis(ax(i), 'right')
+            end
+            switch YAxisKeep(i, j)
+                case 0 % 无标签
+                    ax(i).YTickLabel = {};
+                    ax(i).YLabel = [];
+                case 1 % 左标签
+                    ax(i).YAxisLocation = 'left';
+                case 2 % 右标签
+                    ax(i).YAxisLocation = 'right';
+            end
         end
-        switch YAxisKeep(i, j)
+        switch XAxisKeep(i)
             case 0 % 无标签
-                ax(i).YTickLabel = {};
-                ax(i).YLabel = [];
-            case 1 % 左标签
-                ax(i).YAxisLocation = 'left';
-            case 2 % 右标签
-                ax(i).YAxisLocation = 'right';
+                ax(i).XTickLabel = {};
+                ax(i).XLabel = [];
+            case 1 % 下标签
+                ax(i).XAxisLocation = 'bottom';
+            case 2 % 上标签
+                ax(i).XAxisLocation = 'top';
         end
-    end
-    switch XAxisKeep(i)
-        case 0 % 无标签
-            ax(i).XTickLabel = {};
-            ax(i).XLabel = [];
-        case 1 % 下标签
-            ax(i).XAxisLocation = 'bottom';
-        case 2 % 上标签
-            ax(i).XAxisLocation = 'top';
+    else
+        % 改变当前坐标轴指向
+        axes(ax(i))
+        % 先把m_gird去掉，重新画
+        m_ungrid;
+        YAxisLocationtemp = MAP_GRID.YaxisLocation;
+        XAxisLocationtemp = MAP_GRID.XaxisLocation;
+        switch YAxisKeep(i)
+            case 1 % 左标签
+                YAxisLocationtemp = 'left';
+            case 2 % 右标签
+                YAxisLocationtemp = 'right';
+        end
+        switch XAxisKeep(i)
+            case 1 % 下标签
+                XAxisLocationtemp = 'bottom';
+            case 2 % 上标签
+                XAxisLocationtemp = 'top';
+        end
+        m_grid('box', MAP_GRID.box, ...
+            'xtick', MAP_GRID.xtick, ...
+            'ytick', MAP_GRID.ytick, ...
+            'ticklen', MAP_GRID.ticklen, ...
+            'tickdir', MAP_GRID.tickdir, ...
+            'xlabeldir', MAP_GRID.xlabeldir, ...
+            'ylabeldir', MAP_GRID.ylabeldir, ...
+            'color', MAP_GRID.color, ...
+            'linewidth', MAP_GRID.linewidth, ...
+            'linestyle', MAP_GRID.linestyle, ...
+            'fontsize', MAP_GRID.fontsize, ...
+            'fontname', MAP_GRID.fontname, ...
+            'XaxisLocation', XAxisLocationtemp, ...
+            'YaxisLocation', YAxisLocationtemp);
+        if YAxisKeep(i) == 0
+            delete(findobj(gca, 'Tag', 'm_grid_yticklabel'));
+        end
+        if XAxisKeep(i) == 0
+            delete(findobj(gca, 'Tag', 'm_grid_xticklabel'));
+        end
     end
 end
---
+++ m_AdjudgeTickLabel.m
@@ -0,0 +1,106 @@
+function m_AdjudgeTickLabel(varargin)
+% this function is used to adjudge ticks label rotation for m_map toolbox.
+% Therefore you should install m_map toolbox first.
+%
+% ATTETION: before using this function, check you have inserted following
+% command in the main of function m_grid and call m_grid.
+%   global MAP_GRID
+%   MAP_GRID.box = gbox;
+%   MAP_GRID.xtick = xtick;
+%   MAP_GRID.ytick = ytick;
+%   MAP_GRID.ticklen = gticklen;
+%   MAP_GRID.tickdir = gtickdir;
+%   MAP_GRID.xlabeldir = gxticklabeldir;
+%   MAP_GRID.ylabeldir = gyticklabeldir;
+%   MAP_GRID.color = gcolor;
+%   MAP_GRID.linewidth = glinewidth;
+%   MAP_GRID.linestyle = glinestyle;
+%   MAP_GRID.fontsize = gfontsize;
+%   MAP_GRID.fontname = gfontname;
+%   MAP_GRID.XaxisLocation = gxaxisloc;
+%   MAP_GRID.YaxisLocation = gyaxisloc;
+% it should be inserted after read the varargin in the m_grid (about Line
+% 176).
+%
+%% Syntax
+% m_AdjudgeTickLabel
+% m_AdjudgeTickLabel(axis);
+% m_AdjudgeTickLabel(axis, rotation);
+%
+%% varargin lists
+% NAME & VALUE
+%   axis            'x' or 'y', to tell us which axis you want to adjudge.
+%                   if you inout nothing, 'x' and 'y', will be defult.
+%   rotation        the rotation of ticks label. it should follow the axis
+%                   so that we can know what the rotation belongs to.
+%
+%% example
+% figure
+% m_proj('Lambert Conformal Conic', 'lon', [160, 180], 'lat', [-70, -65])
+% m_grid
+% m_AdjudgeTickLabel('x', 'y', 90)
+
+%%
+global MAP_GRID
+if isempty(MAP_GRID) % 必须做过了m_grid
+    error('you should insert VAR MAP_GRID and call m_grid. See more in help.')
+end
+
+% 默认x，y都做
+if nargin == 0
+    varargin = {'x', 'y'};
+end
+
+for i = 1 : length(varargin)
+    switch varargin{i}
+        case 'x' % x轴
+            % 如果x后面有输入且是数字，那个数字就是rotation，否则默认为0
+            if i + 1 <= length(varargin) && isnumeric(varargin{i + 1})
+                Rotation = varargin{i + 1};
+            else
+                Rotation = 0;
+            end
+            
+            Ticks = findobj(gca, 'Tag', 'm_grid_xticklabel');
+            for j = 1 : length(Ticks)
+                % 旋转
+                Ticks(j).Rotation = Rotation;
+                % 对于旋转家督过大的情况改变旋转锚点位置，防止入侵坐标轴
+                if abs(Rotation - 90) <= 75
+                    % 锚点位置要根据x轴在左在右决定
+                    if isequal(MAP_GRID.XaxisLocation, 'bottom')
+                        Ticks(j).HorizontalAlignment = 'right';
+                    elseif isequal(MAP_GRID.XaxisLocation, 'top')
+                        Ticks(j).HorizontalAlignment = 'left';
+                    elseif isequal(MAP_GRID.XaxisLocation, 'middle')
+                        Ticks(j).HorizontalAlignment = 'center';
+                    end
+                    % 但是只要是旋转过大，锚点y轴位置可以固定在中间
+                    Ticks(j).VerticalAlignment = 'middle';
+                end
+            end
+            
+        case 'y' % 与x轴类似
+            if i + 1 <= length(varargin) && isnumeric(varargin{i + 1})
+                Rotation = varargin{i + 1};
+            else
+                Rotation = 0;
+            end
+            
+            Ticks = findobj(gca, 'Tag', 'm_grid_yticklabels');
+            for j = 1 : length(Ticks)
+                Ticks(j).Rotation = Rotation;
+                if abs(Rotation - 90) <= 15
+                    Ticks(j).HorizontalAlignment = 'center';
+                    if isequal(MAP_GRID.YaxisLocation, 'left')
+                        Ticks(j).VerticalAlignment = 'bottom';
+                    elseif isequal(MAP_GRID.YaxisLocation, 'right')
+                        Ticks(j).VerticalAlignment = 'top';
+                    elseif isequal(MAP_GRID.YaxisLocation, 'middle')
+                        Ticks(j).VerticalAlignment = 'middle';
+                    end
+                end
+            end
+    end
+end
+end
